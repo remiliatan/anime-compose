a1ee14211fdbe28cdc9bad870da0baa24c069982
Noval Raihan <novalraihan74@gmail.com>
2025-10-29 15:53:25 +0700

    modify circle search indicator

--- app/src/main/java/com/example/animelist/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/animelist/ui/screens/HomeScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -18,6 +19,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -68,7 +70,9 @@ fun HomeScreen(navController:NavController){
                     singleLine = true
                 )
                 if(isLoading){
-                    CircularProgressIndicator()
+                    Column (horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxWidth()){
+                        CircularProgressIndicator(modifier = Modifier.size(24.dp))
+                    }
                 }else{
                     Column{
                         LazyVerticalGrid(
